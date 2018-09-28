0d44a240d435e9d0334e6ff0f6d037c9970bc4b6
adika-juniarsa <juniarsa.adika@gmail.com>
2018-09-28 14:49:50 +0700

    added home activity

--- app/src/main/java/com/adorableproject/pushjaw/adorable_projects_warehouse/MainActivity.java
+++ app/src/main/java/com/adorableproject/pushjaw/adorable_projects_warehouse/MainActivity.java
@@ -2,6 +2,18 @@ package com.adorableproject.pushjaw.adorable_projects_warehouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        AndroidNetworking.initialize(getApplicationContext());
+        AndroidNetworking.enableLogging();
+        AndroidNetworking.get("http://trelevant.herokuapp.com/api/v1/keychain/generate/")
+                .addQueryParameter("appkey", "vBUpPXE8ddpWp9QJ")
+                .setTag("test")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("response is", response.toString());
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d("response is", anError.toString());
+                    }
+                });
+
     }
 }
