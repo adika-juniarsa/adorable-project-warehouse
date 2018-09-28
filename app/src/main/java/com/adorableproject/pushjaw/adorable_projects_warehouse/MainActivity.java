75b88dea7e0675f7cd1e91e1da535ff65c6a7d4c
adika-juniarsa <juniarsa.adika@gmail.com>
2018-09-28 15:56:32 +0700

    added home activity

--- app/src/main/java/com/adorableproject/pushjaw/adorable_projects_warehouse/MainActivity.java
+++ app/src/main/java/com/adorableproject/pushjaw/adorable_projects_warehouse/MainActivity.java
@@ -1,8 +1,11 @@
 package com.adorableproject.pushjaw.adorable_projects_warehouse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
@@ -42,5 +45,13 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+        final Button buttonLogin = (Button) findViewById(R.id.loginBtn);
+        buttonLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(v.getContext(), HomeActivity.class));
+            }
+        });
+
     }
+
 }
